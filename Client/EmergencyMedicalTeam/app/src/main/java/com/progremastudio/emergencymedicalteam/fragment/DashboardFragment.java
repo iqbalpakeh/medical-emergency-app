1b71324822a3e07432b725f0c61916cabbb6bc26
Mochammed Iqbal Pakeh <iqbalpakeh@gmail.com>
2017-02-27 06:28:33 +0700

    Add floating action button sample

--- Client/EmergencyMedicalTeam/app/src/main/java/com/progremastudio/emergencymedicalteam/fragment/DashboardFragment.java
+++ Client/EmergencyMedicalTeam/app/src/main/java/com/progremastudio/emergencymedicalteam/fragment/DashboardFragment.java
@@ -28,7 +28,6 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.database.DataSnapshot;
