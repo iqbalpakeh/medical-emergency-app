f112e7d486651dfff45f83fa144dd42ae2aaac5a
Mochammed Iqbal Pakeh <iqbalpakeh@gmail.com>
2017-03-02 11:53:01 +0700

    Improve dashboard fragment

--- Client/EmergencyMedicalTeam/app/src/main/java/com/progremastudio/emergencymedicalteam/fragment/DashboardFragment.java
+++ Client/EmergencyMedicalTeam/app/src/main/java/com/progremastudio/emergencymedicalteam/fragment/DashboardFragment.java
@@ -12,14 +12,10 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -72,13 +68,13 @@ public class DashboardFragment extends Fragment implements
 
     private String mAddressOutput;
 
-    private EditText mEditText;
-
-    private Button mSubmitButton;
-
-    private Button mFetchLocationButton;
-
-    private TextView mAddressTextView;
+//    private EditText mEditText;
+//
+//    private Button mSubmitButton;
+//
+//    private Button mFetchLocationButton;
+//
+//    private TextView mAddressTextView;
 
     @Nullable
     @Override
@@ -100,25 +96,25 @@ public class DashboardFragment extends Fragment implements
         mMapView.onCreate(savedInstanceState);
         mMapView.getMapAsync(this);
 
-        mEditText = (EditText) rootView.findViewById(R.id.edit_text);
-
-        mSubmitButton = (Button) rootView.findViewById(R.id.submit_code);
-        mSubmitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                submit();
-            }
-        });
-
-        mFetchLocationButton = (Button) rootView.findViewById(R.id.fetch_address_button);
-        mFetchLocationButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fetchLocationAddress();
-            }
-        });
-
-        mAddressTextView = (TextView) rootView.findViewById(R.id.address_text);
+//        mEditText = (EditText) rootView.findViewById(R.id.edit_text);
+//
+//        mSubmitButton = (Button) rootView.findViewById(R.id.submit_code);
+//        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                submit();
+//            }
+//        });
+//
+//        mFetchLocationButton = (Button) rootView.findViewById(R.id.fetch_address_button);
+//        mFetchLocationButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                fetchLocationAddress();
+//            }
+//        });
+//
+//        mAddressTextView = (TextView) rootView.findViewById(R.id.address_text);
 
         return rootView;
     }
@@ -138,7 +134,7 @@ public class DashboardFragment extends Fragment implements
             }
             if (savedInstanceState.keySet().contains(LOCATION_ADDRESS_KEY)) {
                 mAddressOutput = savedInstanceState.getString(LOCATION_ADDRESS_KEY);
-                mAddressTextView.setText(mAddressOutput);
+                //mAddressTextView.setText(mAddressOutput);
             }
         }
     }
@@ -283,12 +279,12 @@ public class DashboardFragment extends Fragment implements
 
     private void submit() {
 
-        final String content = mEditText.getText().toString();
-
-        if (TextUtils.isEmpty(content)) {
-            mEditText.setError("Required");
-            return;
-        }
+        final String content = ""; //mEditText.getText().toString();
+//
+//        if (TextUtils.isEmpty(content)) {
+//            mEditText.setError("Required");
+//            return;
+//        }
 
         setEditingEnabled(false);
         Toast.makeText(getActivity(), "Posting...", Toast.LENGTH_SHORT).show();
@@ -325,12 +321,12 @@ public class DashboardFragment extends Fragment implements
     }
 
     private void setEditingEnabled(boolean enabled) {
-        mEditText.setEnabled(enabled);
-        if (enabled) {
-            mSubmitButton.setVisibility(View.VISIBLE);
-        } else {
-            mSubmitButton.setVisibility(View.GONE);
-        }
+//        mEditText.setEnabled(enabled);
+//        if (enabled) {
+//            mSubmitButton.setVisibility(View.VISIBLE);
+//        } else {
+//            mSubmitButton.setVisibility(View.GONE);
+//        }
     }
 
     private void writeNewPost(String userId, String message) {
@@ -376,7 +372,7 @@ public class DashboardFragment extends Fragment implements
         protected void onReceiveResult(int resultCode, Bundle resultData) {
             mAddressOutput = resultData.getString(AddressService.Constants.RESULT_DATA_KEY);
             Log.d(TAG, "Address = " + mAddressOutput);
-            mAddressTextView.setText(mAddressOutput);
+            //mAddressTextView.setText(mAddressOutput);
             if (resultCode == AddressService.Constants.SUCCESS_RESULT) {
                 ((BaseActivity) getActivity()).hideProgressDialog();
                 Log.d(TAG, "Address found");
