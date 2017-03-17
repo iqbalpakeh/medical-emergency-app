09abd681040ceafede4bf7eaea4a49fb2954022a
Progrema Studio <progrema.studio@gmail.com>
2017-03-17 20:37:56 +0800

    Add comment to AppContext class

--- Client/EmergencyMedicalTeam/app/src/main/java/com/progremastudio/emergencymedicalteam/AppContext.java
+++ Client/EmergencyMedicalTeam/app/src/main/java/com/progremastudio/emergencymedicalteam/AppContext.java
@@ -25,6 +25,14 @@ public class AppContext {
         storeCurrentUser(context, "", "", "");
     }
 
+    /**
+     * Store current user's display name, email and phone number
+     *
+     * @param context application context
+     * @param displayName current user's display name
+     * @param email current user's email
+     * @param phoneNumber current user's phone number
+     */
     public static void storeCurrentUser(Context context,
                                         String displayName,
                                         String email,
@@ -35,6 +43,12 @@ public class AppContext {
         storeCurrentUserPhoneNumber(context, phoneNumber);
     }
 
+    /**
+     * Store current user's display name to shared-preference
+     *
+     * @param context application context
+     * @param displayName current user's display name
+     */
     public static void storeCurrentUserDisplayName(Context context, String displayName) {
         SharedPreferences appContext = context.getSharedPreferences(APP_CONTEXT, 0);
         SharedPreferences.Editor editor = appContext.edit();
@@ -42,11 +56,23 @@ public class AppContext {
         editor.commit();
     }
 
+    /**
+     * Fetch current user's display name from shared-preference
+     *
+     * @param context application context
+     * @return current user's display name
+     */
     public static String fetchCurrentUserDisplayName(Context context) {
         SharedPreferences appContext = context.getSharedPreferences(APP_CONTEXT, 0);
         return appContext.getString(USER_DISPLAY_NAME, "");
     }
 
+    /**
+     * Store current user's email to shared-preference
+     *
+     * @param context application context
+     * @param email current user's email
+     */
     public static void storeCurrentUserEmail(Context context, String email) {
         SharedPreferences appContext = context.getSharedPreferences(APP_CONTEXT, 0);
         SharedPreferences.Editor editor = appContext.edit();
@@ -54,11 +80,23 @@ public class AppContext {
         editor.commit();
     }
 
+    /**
+     * Fetch current user's email from shared-preference
+     *
+     * @param context application context
+     * @return current user's email
+     */
     public static String fetchCurrentUserEmail(Context context) {
         SharedPreferences appContext = context.getSharedPreferences(APP_CONTEXT, 0);
         return appContext.getString(USER_EMAIL, "");
     }
 
+    /**
+     * Store current user's phone number to shared-preference
+     *
+     * @param context application context
+     * @param phoneNumber current user's phone number
+     */
     public static void storeCurrentUserPhoneNumber(Context context, String phoneNumber) {
         SharedPreferences appContext = context.getSharedPreferences(APP_CONTEXT, 0);
         SharedPreferences.Editor editor = appContext.edit();
@@ -66,11 +104,23 @@ public class AppContext {
         editor.commit();
     }
 
+    /**
+     * Fetch current user's phone number from shared-preference
+     *
+     * @param context application context
+     * @return current user's phone number
+     */
     public static String fetchCurrentUserPhoneNumber(Context context) {
         SharedPreferences appContext = context.getSharedPreferences(APP_CONTEXT, 0);
         return appContext.getString(USER_PHONE_NUMBER, "");
     }
 
+    /**
+     * Store current user's last latitude location to shared-preference
+     *
+     * @param context application context
+     * @param latitude current's user last latitude location
+     */
     public static void storeCurrentUserLastLatitudeLocation(Context context, String latitude) {
         SharedPreferences appContext = context.getSharedPreferences(APP_CONTEXT, 0);
         SharedPreferences.Editor editor = appContext.edit();
@@ -78,11 +128,23 @@ public class AppContext {
         editor.commit();
     }
 
+    /**
+     * Fetch current user's last latitude location from shared-preference
+     *
+     * @param context application context
+     * @return current user's last latitude location
+     */
     public static String fetchCurrentUserLastLatitudeLocation(Context context) {
         SharedPreferences appContext = context.getSharedPreferences(APP_CONTEXT, 0);
         return appContext.getString(LAST_LOCATION_LATITUDE, DEFAULT_LATITUDE);
     }
 
+    /**
+     * Store current user's last longitude location to shared-preference
+     *
+     * @param context application context
+     * @param longitude current user's last longitude location
+     */
     public static void storeCurrentUserLastLongitudeLocation(Context context, String longitude) {
         SharedPreferences appContext = context.getSharedPreferences(APP_CONTEXT, 0);
         SharedPreferences.Editor editor = appContext.edit();
@@ -90,6 +152,12 @@ public class AppContext {
         editor.commit();
     }
 
+    /**
+     * Fetch current user's last longitude location from shared-preference
+     *
+     * @param context application context
+     * @return current user's last longitude location
+     */
     public static String fetchCurrentUserLastLongitudeLocation(Context context) {
         SharedPreferences appContext = context.getSharedPreferences(APP_CONTEXT, 0);
         return appContext.getString(LAST_LOCATION_LONGITUDE, DEFAULT_LONGITUDE);
