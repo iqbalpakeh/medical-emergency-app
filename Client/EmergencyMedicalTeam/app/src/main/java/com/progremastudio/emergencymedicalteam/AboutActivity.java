c78cbb916a90a520e26c87ab0e7dcb80d3a1d810
Progrema Studio <progrema.studio@gmail.com>
2017-03-18 08:17:38 +0800

    Add about activity

---
+++ Client/EmergencyMedicalTeam/app/src/main/java/com/progremastudio/emergencymedicalteam/AboutActivity.java
@@ -0,0 +1,40 @@
+package com.progremastudio.emergencymedicalteam;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.TextView;
+
+public class AboutActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        /*
+        Set activity layout
+         */
+        setContentView(R.layout.activity_splash_screen);
+
+        /*
+        Set application version
+         */
+        try {
+            String packageName = getPackageName();
+            String version = getPackageManager().getPackageInfo(packageName, 0).versionName;
+            TextView applicationVersion = (TextView) findViewById(R.id.app_version);
+            applicationVersion.setText(getString(R.string.str_Version) + version);
+        } catch (PackageManager.NameNotFoundException error) {
+            Log.e("ERROR", error.getStackTrace().toString());
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
