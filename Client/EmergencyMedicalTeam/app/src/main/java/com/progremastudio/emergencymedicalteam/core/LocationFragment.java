469e559a4f95cc54ad52ad5225bc384ceef7c74d
Progrema Studio <progrema.studio@gmail.com>
2017-04-02 07:54:26 +0800

    Implement dummy search view

--- Client/EmergencyMedicalTeam/app/src/main/java/com/progremastudio/emergencymedicalteam/core/LocationFragment.java
+++ Client/EmergencyMedicalTeam/app/src/main/java/com/progremastudio/emergencymedicalteam/core/LocationFragment.java
@@ -3,7 +3,7 @@ package com.progremastudio.emergencymedicalteam.core;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.res.Resources;
+import android.graphics.Color;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
@@ -13,14 +13,23 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.arlib.floatingsearchview.FloatingSearchView;
+import com.arlib.floatingsearchview.suggestions.SearchSuggestionsAdapter;
+import com.arlib.floatingsearchview.suggestions.model.SearchSuggestion;
+import com.arlib.floatingsearchview.util.Util;
 import com.directions.route.Route;
 import com.directions.route.RouteException;
 import com.directions.route.Routing;
@@ -33,7 +42,6 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
@@ -42,6 +50,10 @@ import com.progremastudio.emergencymedicalteam.AddressService;
 import com.progremastudio.emergencymedicalteam.AppSharedPreferences;
 import com.progremastudio.emergencymedicalteam.BaseActivity;
 import com.progremastudio.emergencymedicalteam.R;
+import com.progremastudio.emergencymedicalteam.locationsearch.SearchResultsListAdapter;
+import com.progremastudio.emergencymedicalteam.locationsearch.data.ColorSuggestion;
+import com.progremastudio.emergencymedicalteam.locationsearch.data.ColorWrapper;
+import com.progremastudio.emergencymedicalteam.locationsearch.data.DataHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -58,6 +70,8 @@ public class LocationFragment extends Fragment implements RoutingListener,
     private static final int PERMISSION_TO_ENABLE_GMAP_LOCATION = 2;
 
     private static final int MAP_ANIMATION_DURATION = 1000;
+
+    private static final long FIND_SUGGESTION_SIMULATED_DELAY = 250;
 
     private static final int[] COLORS = new int[]{
             R.color.primary_dark,
@@ -83,7 +97,13 @@ public class LocationFragment extends Fragment implements RoutingListener,
 
     private List<Polyline> mPolylines;
 
-    FloatingSearchView mSearchView;
+    private FloatingSearchView mSearchView;
+
+    private RecyclerView mSearchResultsList;
+
+    private SearchResultsListAdapter mSearchResultsAdapter;
+
+    private String mLastQuery = "";
 
     @Nullable
     @Override
@@ -115,6 +135,7 @@ public class LocationFragment extends Fragment implements RoutingListener,
         /*
         Initialize search view
          */
+        mSearchResultsList = (RecyclerView) rootView.findViewById(R.id.search_results_list);
         mSearchView = (FloatingSearchView) rootView.findViewById(R.id.floating_search_view);
         mSearchView.setOnMenuItemClickListener(new FloatingSearchView.OnMenuItemClickListener() {
             @Override
@@ -132,6 +153,9 @@ public class LocationFragment extends Fragment implements RoutingListener,
                 }
             }
         });
+
+        setupFloatingSearch();
+        setupResultsList();
 
         return rootView;
     }
@@ -228,15 +252,15 @@ public class LocationFragment extends Fragment implements RoutingListener,
         /*
         Update map with retro style
          */
-        try {
-            boolean success = googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(
-                    getContext(), R.raw.retro_style_json));
-            if (!success) {
-                Log.e(TAG, "Style parsing failed.");
-            }
-        } catch (Resources.NotFoundException e) {
-            Log.e(TAG, "Can't find style. Error: ", e);
-        }
+//        try {
+//            boolean success = googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(
+//                    getContext(), R.raw.retro_style_json));
+//            if (!success) {
+//                Log.e(TAG, "Style parsing failed.");
+//            }
+//        } catch (Resources.NotFoundException e) {
+//            Log.e(TAG, "Can't find style. Error: ", e);
+//        }
 
         /*
         Prepare google map object
@@ -564,6 +588,212 @@ public class LocationFragment extends Fragment implements RoutingListener,
             mGoogleMap.getUiSettings().setMyLocationButtonEnabled(false);
         }
     }
+
+    private void setupFloatingSearch() {
+        mSearchView.setOnQueryChangeListener(new FloatingSearchView.OnQueryChangeListener() {
+
+            @Override
+            public void onSearchTextChanged(String oldQuery, final String newQuery) {
+
+                if (!oldQuery.equals("") && newQuery.equals("")) {
+                    mSearchView.clearSuggestions();
+                } else {
+
+                    //this shows the top left circular progress
+                    //you can call it where ever you want, but
+                    //it makes sense to do it when loading something in
+                    //the background.
+                    mSearchView.showProgress();
+
+                    //simulates a query call to a data source
+                    //with a new query.
+                    DataHelper.findSuggestions(getActivity(), newQuery, 5,
+                            FIND_SUGGESTION_SIMULATED_DELAY, new DataHelper.OnFindSuggestionsListener() {
+
+                                @Override
+                                public void onResults(List<ColorSuggestion> results) {
+
+                                    //this will swap the data and
+                                    //render the collapse/expand animations as necessary
+                                    mSearchView.swapSuggestions(results);
+
+                                    //let the users know that the background
+                                    //process has completed
+                                    mSearchView.hideProgress();
+                                }
+                            });
+                }
+
+                Log.d(TAG, "onSearchTextChanged()");
+            }
+        });
+
+        mSearchView.setOnSearchListener(new FloatingSearchView.OnSearchListener() {
+            @Override
+            public void onSuggestionClicked(final SearchSuggestion searchSuggestion) {
+
+                ColorSuggestion colorSuggestion = (ColorSuggestion) searchSuggestion;
+                DataHelper.findColors(getActivity(), colorSuggestion.getBody(),
+                        new DataHelper.OnFindColorsListener() {
+
+                            @Override
+                            public void onResults(List<ColorWrapper> results) {
+                                mSearchResultsAdapter.swapData(results);
+                            }
+
+                        });
+                Log.d(TAG, "onSuggestionClicked()");
+
+                mLastQuery = searchSuggestion.getBody();
+            }
+
+            @Override
+            public void onSearchAction(String query) {
+                mLastQuery = query;
+
+                DataHelper.findColors(getActivity(), query,
+                        new DataHelper.OnFindColorsListener() {
+
+                            @Override
+                            public void onResults(List<ColorWrapper> results) {
+                                mSearchResultsAdapter.swapData(results);
+                            }
+
+                        });
+                Log.d(TAG, "onSearchAction()");
+            }
+        });
+
+        mSearchView.setOnFocusChangeListener(new FloatingSearchView.OnFocusChangeListener() {
+            @Override
+            public void onFocus() {
+
+                //show suggestions when search bar gains focus (typically history suggestions)
+                mSearchView.swapSuggestions(DataHelper.getHistory(getActivity(), 3));
+
+                Log.d(TAG, "onFocus()");
+            }
+
+            @Override
+            public void onFocusCleared() {
+
+                //set the title of the bar so that when focus is returned a new query begins
+                mSearchView.setSearchBarTitle(mLastQuery);
+
+                //you can also set setSearchText(...) to make keep the query there when not focused and when focus returns
+                //mSearchView.setSearchText(searchSuggestion.getBody());
+
+                Log.d(TAG, "onFocusCleared()");
+            }
+        });
+
+
+//        //handle menu clicks the same way as you would
+//        //in a regular activity
+//        mSearchView.setOnMenuItemClickListener(new FloatingSearchView.OnMenuItemClickListener() {
+//            @Override
+//            public void onActionMenuItemSelected(MenuItem item) {
+//
+//                if (item.getItemId() == R.id.action_change_colors) {
+//
+//                    mIsDarkSearchTheme = true;
+//
+//                    //demonstrate setting colors for items
+//                    mSearchView.setBackgroundColor(Color.parseColor("#787878"));
+//                    mSearchView.setViewTextColor(Color.parseColor("#e9e9e9"));
+//                    mSearchView.setHintTextColor(Color.parseColor("#e9e9e9"));
+//                    mSearchView.setActionMenuOverflowColor(Color.parseColor("#e9e9e9"));
+//                    mSearchView.setMenuItemIconColor(Color.parseColor("#e9e9e9"));
+//                    mSearchView.setLeftActionIconColor(Color.parseColor("#e9e9e9"));
+//                    mSearchView.setClearBtnColor(Color.parseColor("#e9e9e9"));
+//                    mSearchView.setDividerColor(Color.parseColor("#BEBEBE"));
+//                    mSearchView.setLeftActionIconColor(Color.parseColor("#e9e9e9"));
+//                } else {
+//
+//                    //just print action
+//                    Toast.makeText(getActivity().getApplicationContext(), item.getTitle(),
+//                            Toast.LENGTH_SHORT).show();
+//                }
+//
+//            }
+//        });
+
+        //use this listener to listen to menu clicks when app:floatingSearch_leftAction="showHome"
+        mSearchView.setOnHomeActionClickListener(new FloatingSearchView.OnHomeActionClickListener() {
+            @Override
+            public void onHomeClicked() {
+
+                Log.d(TAG, "onHomeClicked()");
+            }
+        });
+
+        /*
+         * Here you have access to the left icon and the text of a given suggestion
+         * item after as it is bound to the suggestion list. You can utilize this
+         * callback to change some properties of the left icon and the text. For example, you
+         * can load the left icon images using your favorite image loading library, or change text color.
+         *
+         *
+         * Important:
+         * Keep in mind that the suggestion list is a RecyclerView, so views are reused for different
+         * items in the list.
+         */
+        mSearchView.setOnBindSuggestionCallback(new SearchSuggestionsAdapter.OnBindSuggestionCallback() {
+            @Override
+            public void onBindSuggestion(View suggestionView, ImageView leftIcon,
+                                         TextView textView, SearchSuggestion item, int itemPosition) {
+                ColorSuggestion colorSuggestion = (ColorSuggestion) item;
+
+                String textColor = false ? "#ffffff" : "#000000";
+                String textLight = false ? "#bfbfbf" : "#787878";
+
+                if (colorSuggestion.getIsHistory()) {
+                    leftIcon.setImageDrawable(ResourcesCompat.getDrawable(getResources(),
+                            R.drawable.ic_history_black_24dp, null));
+
+                    Util.setIconColor(leftIcon, Color.parseColor(textColor));
+                    leftIcon.setAlpha(.36f);
+                } else {
+                    leftIcon.setAlpha(0.0f);
+                    leftIcon.setImageDrawable(null);
+                }
+
+                textView.setTextColor(Color.parseColor(textColor));
+                String text = colorSuggestion.getBody()
+                        .replaceFirst(mSearchView.getQuery(),
+                                "<font color=\"" + textLight + "\">" + mSearchView.getQuery() + "</font>");
+                textView.setText(Html.fromHtml(text));
+            }
+
+        });
+
+        //listen for when suggestion list expands/shrinks in order to move down/up the
+        //search results list
+        mSearchView.setOnSuggestionsListHeightChanged(new FloatingSearchView.OnSuggestionsListHeightChanged() {
+            @Override
+            public void onSuggestionsListHeightChanged(float newHeight) {
+                mSearchResultsList.setTranslationY(newHeight);
+            }
+        });
+    }
+
+    private void setupResultsList() {
+        mSearchResultsAdapter = new SearchResultsListAdapter();
+        mSearchResultsList.setAdapter(mSearchResultsAdapter);
+        mSearchResultsList.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+
+//    @Override
+//    public boolean onActivityBackPress() {
+//        //if mSearchView.setSearchFocused(false) causes the focused search
+//        //to close, then we don't want to close the activity. if mSearchView.setSearchFocused(false)
+//        //returns false, we know that the search was already closed so the call didn't change the focus
+//        //state and it makes sense to call supper onBackPressed() and close the activity
+//        if (!mSearchView.setSearchFocused(false)) {
+//            return false;
+//        }
+//        return true;
+//    }
 
     /**
      * Receiver class for Address Fetch Service
