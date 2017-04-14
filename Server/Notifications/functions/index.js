4b8649b31e5aa79a14971148aad0687bf223b8ed
Progrema Studio <progrema.studio@gmail.com>
2017-04-15 01:42:04 +0800

    Update index.js

--- Server/Notifications/functions/index.js
+++ Server/Notifications/functions/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const TAG = 'DBG#5'
+const TAG = 'DBG#7'
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
@@ -16,24 +16,43 @@ admin.initializeApp(functions.config().firebase);
 exports.sendNotification = functions.database.ref('/chat').onWrite(event => {
 
     console.log(TAG, " Start of new functions ====================================")
-
     console.log(TAG, ' New messages are posted by user');
 
     // Get the list of device notification tokens.
-    const getDeviceTokensPromise = admin.database().ref(`/users/d65vrFI5ouTvBsaozx5peKftdPE3/token`).once('value');
+    const getDeviceTokensPromise = admin.database().ref(`/users/RjqlkkTE7YdSw1gM8gFZZ4DzVDG3/token`).once('value');
 
-    // Get the follower profile.
-    const getFollowerProfilePromise = admin.auth().getUser("d65vrFI5ouTvBsaozx5peKftdPE3");
-
-    return Promise.all([getDeviceTokensPromise, getFollowerProfilePromise]).then(results =>{
+    return Promise.all([getDeviceTokensPromise]).then(results => {
         const tokensSnapshot = results[0];
-        const follower = results[1];
 
+        // Notification details.
+        const payload = {
+          notification: {
+            title: 'TBM Chat notification',
+            body: `TBM Chat`
+          }
+        };
+
+        // Listing all tokens
         const tokens = Object.keys(tokensSnapshot.val());
+        console.log(TAG, ' tokensSnapshot.val() ', tokensSnapshot.val());
 
-        console.log(TAG, ' tokensSnapshot.val() = ', tokensSnapshot.val());
-        console.log(TAG, ' follower = ', follower);
+        // Send notificatins to all tokens.
+        return admin.messaging().sendToDevice(tokens, payload).then(response => {
+          // For each message check if there was an error.
+          const tokensToRemove = [];
+          response.results.forEach((result, index) => {
+          const error = result.error;
+          if (error) {
+              console.error('Failure sending notification to', tokens[index], error);
+              // Cleanup the tokens who are not registered anymore.
+              //if (error.code === 'messaging/invalid-registration-token' ||
+              //    error.code === 'messaging/registration-token-not-registered') {
+              //  tokensToRemove.push(tokensSnapshot.ref.child(tokens[index]).remove());
+              //}
+            }
+          });
+          return Promise.all(tokensToRemove);
+        });
 
     });
-
 });
