7a276c1fad9075ecf7a2cf176d8e5e7cf7576e20
Progrema Studio <progrema.studio@gmail.com>
2017-04-14 23:53:03 +0800

    Update index.js

--- Server/Notifications/functions/index.js
+++ Server/Notifications/functions/index.js
@@ -1,8 +1,39 @@
-var functions = require('firebase-functions');
+'use strict';
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+const TAG = 'DBG#5'
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+
+/**
+ * Triggers when a user send chat and then send notification back to user
+ *
+ * This function listen to any change at '/chat'
+ * Users save their token to `/users/{followedUid}/notificationTokens/{notificationToken}`.
+ */
+exports.sendNotification = functions.database.ref('/chat').onWrite(event => {
+
+    console.log(TAG, " Start of new functions ====================================")
+
+    console.log(TAG, ' New messages are posted by user');
+
+    // Get the list of device notification tokens.
+    const getDeviceTokensPromise = admin.database().ref(`/users/d65vrFI5ouTvBsaozx5peKftdPE3/token`).once('value');
+
+    // Get the follower profile.
+    const getFollowerProfilePromise = admin.auth().getUser("d65vrFI5ouTvBsaozx5peKftdPE3");
+
+    return Promise.all([getDeviceTokensPromise, getFollowerProfilePromise]).then(results =>{
+        const tokensSnapshot = results[0];
+        const follower = results[1];
+
+        const tokens = Object.keys(tokensSnapshot.val());
+
+        console.log(TAG, ' tokensSnapshot.val() = ', tokensSnapshot.val());
+        console.log(TAG, ' follower = ', follower);
+
+    });
+
 });
